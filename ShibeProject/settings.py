8d27ec389aeadb4c1df79abfb88fb3ea33a71c6e
Joyder Jovin <140531097+joydeR12@users.noreply.github.com>
2025-04-01 22:08:06 +0300

    Update settings.py

--- ShibeProject/settings.py
+++ ShibeProject/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(t%zhbm%lc6=6t_g9ile%orywi&()99*topa$h+c5p9=(su29&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'vmidebts.com',
+    'www.vmidebts.com',
+    'srv775500',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
@@ -120,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIR = [
